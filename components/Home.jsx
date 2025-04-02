87f07ff9d7153c6f34f58a52fa66f1e7199ef317
Chrisduruh <chrisduruh@gmail.com>
2025-04-03 07:46:49 +0900

    Updated Home.jsx with new layout changes

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,17 +1,6 @@
 export default function Home(){
     return (
     <> 
-    <h2>Who We Are </h2>
-    <p>Our team of dedicated IT professionals combines years of expertise with a passion for technology. We specialize in delivering tailored solutions in software development, cybersecurity, cloud integration, data analytics, IT consulting, and more. Our goal is to help businesses harness the power of technology to unlock their true potential. </p>
-   
-    <h2>Mission Statement</h2>
-    <p>To provide innovative, scalable, and reliable IT solutions that drive business growth, enhance operational efficiency, and ensure long-term success for our clients. We are committed to building strong partnerships based on trust, quality, and innovation. </p>
-    <h2>Why Choose Us?</h2>
-    <p>i. Proven expertise across multiple domains of IT. <br />
-       ii. Personalized solutions tailored to your unique needs. <br />
-        iii.A team dedicated to delivering exceptional results. <br />
-       iv. Let us help you navigate the digital landscape with confidence.</p> 
-       <p> Explore our services and discover how we can transform your business.</p> 
     
     <h3 id="Hhead">Need a Condusive Environment to work in Comfort? We got you! And COFFEE too!!!</h3> <br />
     <img src="cafepictureA.jpg" height={650} width={1200} alt="" /> 
